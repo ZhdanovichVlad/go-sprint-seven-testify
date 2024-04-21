2f604820dc73eb5dba2b2f7f85e79a90cdd94e95
ZhdanovichVlad <zhdanvla@gmail.com>
2024-04-21 22:01:23 +0600

    Add main branch

--- main_test.go
+++ main_test.go
@@ -10,27 +10,13 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// The following function makes the text where the number of cafes is greater than the number of cafes in the cafeList.
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-	require.Equal(t, responseRecorder.Code, http.StatusOK, "Expected status code 200")
+    totalCount := 4
+    req := ... // здесь нужно создать запрос к сервису
 
-	body := responseRecorder.Body.String()
-	assert.NotEmpty(t, responseRecorder.Body, "Body is empty")
-	lenBody := len(strings.Split(body, ","))
-	assert.Equal(t, lenBody, totalCount, "Expected 4 cafe")
-}
+    responseRecorder := httptest.NewRecorder()
+    handler := http.HandlerFunc(mainHandle)
+    handler.ServeHTTP(responseRecorder, req)
 
-// The following function makes the test in which the customer has written the wrong city.
-func TestMainHandlerInWhichWrongCity(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=4&city=minsk", nil)
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest, "Expected status code 400")
-
-}
+    // здесь нужно добавить необходимые проверки
+} 
